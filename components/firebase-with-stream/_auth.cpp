e61fede24653531ee76dd9254fddb51026f4ec11
tharushaudana519@gmail.com <tharushaudana519@gmail.com>
2023-10-26 21:22:01 +0530

    update

--- components/firebase-with-stream/_auth.cpp
+++ components/firebase-with-stream/_auth.cpp
@@ -7,6 +7,7 @@
 #include "esp_tls.h"
 #include "esp_log.h"
 #include "firebase-with-stream.h"
+#include "http_request.h"
 #include "heapless_json_stream_parser.h"
 
 static const char *TAG = "FWS:AUTH";
@@ -36,17 +37,7 @@ void _firebase_auth::_sign_in()
 {
     char *buffer = new char[HTTP_MAX_RECV_BUFFER_SIZE + 1];
 
-    bool failed = false;
-
     std::string url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + config->api_key;
-
-    esp_http_client_config_t config = {
-        .url = url.c_str(),
-        .method = HTTP_METHOD_POST,
-        .crt_bundle_attach = esp_crt_bundle_attach,
-    };
-
-    esp_http_client_handle_t client = esp_http_client_init(&config);
 
     std::string post_data = 
                     "{"
@@ -55,55 +46,37 @@ void _firebase_auth::_sign_in()
                     "\"returnSecureToken\":true"
                     "}";
 
-    ESP_LOGI(TAG, "%s", url.c_str());
+    http_response_result_t r = http_make_request(url.c_str(), post_data.c_str(), HTTP_METHOD_POST, buffer);
 
-    esp_http_client_set_method(client, HTTP_METHOD_POST);
-    esp_http_client_set_header(client, "Content-Type", "application/json");
-    esp_http_client_set_post_field(client, post_data.c_str(), strlen(post_data.c_str()));
-
-    esp_err_t err;
-
-    if ((err = esp_http_client_open(client, post_data.length())) != ESP_OK) {
-        ESP_LOGE(TAG, "[SIGN-IN] >>> Failed to open HTTP connection: %s", esp_err_to_name(err));
-        failed = true;
+    if (r.failed) {
+        ESP_LOGE(TAG, "[SIGN-IN] >>> Failed to open HTTP connection!");
+        delete[] buffer;
+        return;
     }
 
-    int status_code = -1;
-    int content_length = -1;
-
-    if (!failed)
+    if (r.status_code != 200)
     {
-        esp_http_client_write(client, post_data.c_str(), post_data.length());
-        content_length = esp_http_client_fetch_headers(client);
-        status_code = esp_http_client_get_status_code(client);
+        ESP_LOGE(TAG, "[SIGN-IN] >>> Response failed with code %d", r.status_code);
+        size_t s = esp_get_free_heap_size();
+        ESP_LOGI(TAG, "free heap: %u", s);
+        delete[] buffer;
+        return;
     }
 
-    if (!failed && status_code != 200)
+    if (r.read_len > 0)
     {
-        ESP_LOGE(TAG, "[SIGN-IN] >>> Response failed with code %d", status_code);
-        failed = true;
-    }
-
-    //### read...
-
-    if (!failed)
-    {
-        int read_len = esp_http_client_read(client, buffer, HTTP_MAX_RECV_BUFFER_SIZE);
-
-        if (read_len > 0)
+        /*for (uint16_t i = 0; i < r.read_len; i++)
         {
-            for (uint16_t i = 0; i < read_len; i++)
+            if (jparser.parse(buffer[i]))
             {
-                if (jparser.parse(buffer[i]))
-                {
-                    printf("%s --> %s \n", jparser.path.c_str(), jparser.value.val.c_str());
-                }
+                printf("%s --> %s \n", jparser.path.c_str(), jparser.value.val.c_str());
             }
-        }        
+        }*/
     }
 
-    esp_http_client_close(client);
-    esp_http_client_cleanup(client);
+    size_t s = esp_get_free_heap_size();
+    ESP_LOGI(TAG, "free heap: %u", s);
+
     delete[] buffer;
 }
 
---
+++ components/firebase-with-stream/include/http_request.h
@@ -0,0 +1,71 @@
+struct http_response_result_t
+{
+    bool failed = false;
+    int status_code = -1;
+    int content_length = -1;
+    int read_len = -1;
+};
+
+http_response_result_t http_make_request(const char* url, const char* data, esp_http_client_method_t method, char *&buffer)
+{
+    http_response_result_t result;
+
+    bool failed = false;
+
+    esp_http_client_config_t config = {
+        .url = url,
+        .method = method,
+        .crt_bundle_attach = esp_crt_bundle_attach,
+    };
+
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    esp_http_client_set_header(client, "Content-Type", "application/json");
+    esp_http_client_set_post_field(client, data, strlen(data));
+
+    esp_err_t err;
+
+    if ((err = esp_http_client_open(client, strlen(data))) != ESP_OK) {
+        failed = true;
+    }
+
+    int status_code = -1;
+    int content_length = -1;
+
+    if (!failed)
+    {
+        esp_http_client_write(client, data, strlen(data));
+        result.content_length = esp_http_client_fetch_headers(client);
+        result.status_code = esp_http_client_get_status_code(client);
+    }
+
+    //### read...
+
+    if (!failed)
+    {
+        result.read_len = esp_http_client_read(client, buffer, HTTP_MAX_RECV_BUFFER_SIZE);
+    }
+
+    esp_http_client_close(client);
+    esp_http_client_cleanup(client);
+
+    result.failed = failed;
+
+    return result;
+}
+
+/*bool http_print_result_errors(const char* sub_tag, http_response_result_t r)
+{
+    if (r.failed) {
+        ESP_LOGE(TAG, "[%s] >>> Failed to open HTTP connection!", sub_tag);
+        return true;
+    }
+
+    if (r.status_code != 200)
+    {
+        ESP_LOGE(TAG, "[%s] >>> Response failed with code %d", sub_tag, r.status_code);
+        return true;
+    }
+
+    return false;
+}*/
